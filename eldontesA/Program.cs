e48d799f27a0fd93e4f8efa29cfea8b6c7a3099e
szabomarcel <szabomarcell34@gmail.com>
2024-01-16 10:45:48 +0100

    Add project files.

---
+++ eldontesA/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eldontesA
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Eldöntés "A"
+            // Eldöntés az összes elem vizsgálatával
+            int n;
+
+            n = (int)inputValue();
+            int[] tomb = new int[n];
+            int i;
+
+            for (i = 0; i <= n - 1; i++)
+            {
+                tomb[i] = (int)inputValue();
+            }
+            int szam;
+
+            szam = (int)inputValue();
+            int k;
+
+            k = 0;
+            for (i = 0; i <= n - 1; i++)
+            {
+                if (tomb[i] == szam)
+                {
+                    k = k + 1;
+                }
+            }
+            if (k == 0)
+            {
+                Console.WriteLine("Nincs benne");
+            }
+            else
+            {
+                Console.WriteLine("Benne van");
+            }
+            Console.ReadLine();
+        }
+
+        // .NET can only read single characters or entire lines from the
+        // console. The following function safely reads a double value.
+        private static double inputValue()
+        {
+            double result;
+            while (!double.TryParse(Console.ReadLine(), out result)) ;
+            return result;
+        }
+    }
+}
---
+++ megszamolas/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace megszamolas
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Megszámolás
+            // 
+            // Adott egy N elemű T számsorozat. Határozzuk meg az adott tulajdonsággal rendelkező elemek számát!
+            // A végeredményt az 'szamlalo' tartalmazza.
+            int szamlalo;
+
+            szamlalo = 0;
+            int n;
+
+            n = (int)inputValue();
+            int i;
+            int[] t = new int[n];
+
+            for (i = 0; i <= n - 1; i++)
+            {
+                t[i] = (int)inputValue();
+            }
+            for (i = 0; i <= n - 1; i++)
+            {
+                if (t[i] < 0)
+                {
+                    szamlalo = szamlalo + 1;
+                }
+            }
+            Console.WriteLine(szamlalo);
+            Console.ReadLine();
+        }
+
+        // .NET can only read single characters or entire lines from the
+        // console. The following function safely reads a double value.
+        private static double inputValue()
+        {
+            double result;
+            while (!double.TryParse(Console.ReadLine(), out result)) ;
+            return result;
+        }
+    }
+
+}
